e5f78204ee41f03a2265018a8b4ee2606c1d141e
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-05 14:06:59 +0530

    Update events.py

--- PikachuOpRobot/events.py
+++ PikachuOpRobot/events.py
@@ -6,12 +6,12 @@ def register(**args):
     """ Registers a new message. """
     pattern = args.get('pattern', None)
 
-    r_pattern = r'^[/!]'
+    r_pattern = r'^[/!%]'
 
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
 
-    args['pattern'] = pattern.replace('^/', r_pattern, 1)
+    args['pattern'] = pattern.replace('/!%', r_pattern, 1)
 
     def decorator(func):
         client.add_event_handler(func, events.NewMessage(**args))
